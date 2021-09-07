b199b673f5259721e6e6cc843bb880b7a887de43
Brad Johansen <brad.d.johansen@gmail.com>
2021-09-07 14:24:54 -0500

    standard npm i, env config, started on db.sql file

--- database.sql
+++ database.sql
@@ -3,8 +3,24 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
+-- Written in camelCase in honor of my instructor :apple:
+
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "name" VARCHAR (255),
+    "createdOn" TIMESTAMP DEFAULT NOW() NOT NULL
 );
+
+CREATE TABLE "anticipation" (
+    "id" serial PRIMARY KEY,
+    "name" varchar(255) NOT NULL
+);
+
+CREATE TABLE "contact" (
+    "id" SERIAL PRIMARY KEY,
+    "userIdA" INT REFERENCES "user"."id",
+    "userIdB" INT REFERENCES ""
+)
