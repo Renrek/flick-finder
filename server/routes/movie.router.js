50bec65751a0eb35147e4baf0d949efd0eb62afd
Brad Johansen <brad.d.johansen@gmail.com>
2021-09-09 08:04:26 -0500

    api route established, tested with postman

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios');
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
@@ -6,7 +7,23 @@ const router = express.Router();
  * GET route template
  */
 router.get('/', (req, res) => {
-  // GET route code here
+  axios({
+    method: 'GET',
+    url: 'https://api.themoviedb.org/3/search/movie',
+    params: {
+      api_key: process.env.TMDB_API_KEY,
+      language: 'en-US',
+      include_adult: 'false',
+      query: 'Star Wars'
+    }
+  })
+  .then(response => {
+    console.log('axios response', response.data);
+    res.send(response.data);
+  }).catch(err => {
+    console.log('err',err);
+    
+  });
 });
 
 /**
