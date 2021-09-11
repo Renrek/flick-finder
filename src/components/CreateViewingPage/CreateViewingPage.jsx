1dd8f029914196cde0c7d9234211f58cab08ea52
Brad Johansen <brad.d.johansen@gmail.com>
2021-09-11 10:40:45 -0500

    create viewing page roughed out

--- src/components/CreateViewingPage/CreateViewingPage.jsx
+++ src/components/CreateViewingPage/CreateViewingPage.jsx
@@ -1,10 +1,51 @@
+/**** SYSTEM ****/
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+/**** MATERIAL UI ****/
+import { makeStyles } from '@material-ui/core';
+import { 
+    Paper,
+    Typography,
+    TextField,
+    Button
+ } from '@material-ui/core';
+
+import FaceIcon from '@material-ui/icons/Face';
+import SearchIcon from '@material-ui/icons/Search';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+
 
 const CreateViewingPage = () => {
+
+    const [viewers, setViewers] = React.useState([])
+    const contacts = useSelector(store => store.contacts);
+
     return (
         <div>
-           
-            
+            <Paper>
+                <Button>NOW</Button>
+                <form  noValidate>
+                    <TextField
+                        id="datetime-local"
+                        label="Next appointment"
+                        type="datetime-local"
+                        defaultValue="2017-05-24T10:30"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        
+                    />
+                    
+                </form>
+                
+            </Paper>
+           <Paper>
+            {contacts.map((contact) => (
+                <Button key={contact.id} variant="contained" color="primary" style={{ width: '100%'}}>{contact.username}</Button>
+            ))}  
+            </Paper>
+            <Button>FIND MY MOVIE</Button>
         </div>
     )
 }
